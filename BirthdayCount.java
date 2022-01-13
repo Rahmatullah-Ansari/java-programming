1a30c5ad7390b708e09408e896d7b792ca8eab58
Riyaaz <rahmatullaha845@gmail.com>
2022-01-13 18:14:35 +0530

    JAVA-0.0.2

--- BirthdayCount.java
+++ BirthdayCount.java
@@ -1,13 +1,15 @@
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Period;
 import java.util.Date;
 
 class BirthdayCount{
     public static void main(String[] args) {
         String array[]=getCurrentDate().split(":");
-        LocalDate date1=LocalDate.of(1999,12,21);
-        LocalDate date2=LocalDate.of(Integer.parseInt(array[2]),Integer.parseInt(array[1]),Integer.parseInt(array[0]));
+        LocalDate date2=LocalDate.of(1999,12,21);
+        //LocalDate date2=LocalDate.of(Integer.parseInt(array[2]),Integer.parseInt(array[1]),Integer.parseInt(array[0]));
+        LocalDate date1=date2.with(Month.JANUARY).withDayOfMonth(Integer.parseInt(array[0]));
         Period p =Period.between(date1, date2);
         System.out.println("Years = "+p.getYears());
         System.out.println("Months = "+p.getMonths());
